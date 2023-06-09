3b76327ee6dee0f569bdd33742ca5e08b17a4331
lex0819 <lexmolnar@gmail.com>
2023-06-09 18:56:56 +0300

    ownersort

--- lesson04/ownersort.sh
+++ lesson04/ownersort.sh
@@ -8,14 +8,14 @@ folder=$(ls -l | tr -s ' ' '\t' | cut -f '3 9')
 count=0
 for i in $folder; do
     count=$((count+1))
-        if (($count%2))
-            then
-                dir=$i
-                echo $dir = $i zero
-            else
-                if [ -f ./$i ]
-                    then
-                    cp ./$i ./$dir/$i
-                fi
-        fi
+    if (($count%2))
+        then
+            dir=$i
+            echo $dir was checked
+        else
+            if [ -f ./$i ]
+                then
+                cp ./$i ./$dir/$i
+            fi
+    fi
 done
